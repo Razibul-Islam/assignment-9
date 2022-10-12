13330ce26bd331fabffd72621110f7e08887f26a
Razibul Islam <razibulislam665@gmail.com>
2022-10-13 01:32:30 +0600

    error page added

--- src/Components/Error/Error.js
+++ src/Components/Error/Error.js
@@ -3,9 +3,21 @@ import { Link } from 'react-router-dom';
 
 const Error = () => {
     return (
-        <div>
-            
-      </div>
+      <main class="h-screen w-full flex flex-col justify-center items-center bg-gray-600">
+        <h1 class="text-9xl font-extrabold text-white tracking-widest">404</h1>
+        <div class="bg-black text-white px-2 text-sm rounded rotate-12 absolute">
+          Page Not Found
+        </div>
+        <button class="mt-5">
+          <Link to='/' class="relative inline-block text-sm font-medium text-white group active:text-white focus:outline-none focus:ring">
+            <span class="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-gray-400 group-hover:translate-y-0 group-hover:translate-x-0"></span>
+
+            <span class="relative block px-8 py-3 bg-black border border-current">
+              <router-link to="/">Go Home</router-link>
+            </span>
+          </Link>
+        </button>
+      </main>
     );
 };
 
