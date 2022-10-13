7e6fa76b54bc969c84b9c9f78c9c70cb5618b2cc
Razibul Islam <razibulislam665@gmail.com>
2022-10-14 04:05:36 +0600

    everything done

--- src/Components/InnerQuiz/InnerQuiz.js
+++ src/Components/InnerQuiz/InnerQuiz.js
@@ -8,7 +8,8 @@ const InnerQuiz = () => {
   const { id, logo, name, questions, total } = data;
   return (
     <div>
-      <h1 className="text-7xl font-semibold text-center my-5">Start Quiz</h1>
+      <h1 className="text-7xl font-semibold text-center my-5">Start {name} Quiz</h1>
+      {/* <h2 className="text-4xl font-semibold text-center my-5">Quiz of { name}</h2> */}
       {questions.map((question) => (
         <Quiz key={id} questions={question}></Quiz>
       ))}
